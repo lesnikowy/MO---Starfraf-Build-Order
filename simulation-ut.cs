3e05b975d0c8366904afba0a1a55006280c34a6c
lesnikowy <grzegorzcisek@gmail.com>
2014-09-05 23:52:04 +0200

    Create simulation-ut.cs

---
+++ simulation-ut.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarcraftBuildOrderApp
+{
+    class UnitTest {
+        static void Main(string[] args) {
+            bool finished;
+
+            simulation.sim sim_unit = new simulation.sim();
+	        uint score;
+	        // do not include starting units: SCVs and Command Center
+            List<simulation.unit_type> bld_order = new List<simulation.unit_type>();
+
+            bld_order.Add(simulation.unit_type.SCV);
+            bld_order.Add(simulation.unit_type.BARRACKS);
+            bld_order.Add(simulation.unit_type.BARRACKS);
+            bld_order.Add(simulation.unit_type.MARINE);
+            bld_order.Add(simulation.unit_type.MARINE);
+            bld_order.Add(simulation.unit_type.ACADEMY);	
+
+            score = sim_unit.run(bld_order);
+
+            Console.WriteLine("INFO: Build order compleated with time " + score);
+
+            System.Threading.Thread.Sleep(5000);
+        }
+    }
+}
