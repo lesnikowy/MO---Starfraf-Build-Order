21fcb99086dfae72fd225d93bd2be329db536384
Mateusz Knapik <mateuszknapik@gmail.com>
2014-09-09 22:04:30 +0200

    Nowy początek

---
+++ StarcraftBuildOrderApp/tabusearch/TabuSearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StarcraftBuildOrderApp.tabusearch
+{
+	public class TabuSearch
+	{
+		public TabuSearch ()
+		{
+		}
+
+		public void iterate()
+		{
+			// zrob sasiadow
+			// sprawdz wszystkich sasiadow, ale nie tych co sa na TL
+			// j.w. ale tylko tych z TL
+			// update tabu
+			// dodaj najlepszy ruch do tabu
+			// jesli ten ruch najelpszy z najlepszych to podmien globalne minimum
+			// jesli ukaszowe lepsze niz aktualne to bieremy je do nastepnej iteracji i jako globalne minimum
+			// psy szczekaja, karawana jedzie dalej
+		}
+
+		private void createNeighbours()
+		{
+			// jakims cudem stworz sasiadow
+		}
+
+		private void evaluateSolution()
+		{
+			// wez i wywolaj zajebiste funkcje ukaszowe
+		}
+
+	}
+}
+
