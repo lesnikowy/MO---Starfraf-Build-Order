7862f8391119380c9d4f9df0a51c687c3023ce02
Łukasz <Waligrski>
2014-09-10 23:15:42 +0200

    dodano proste wymagania

--- StarcraftBuildOrderApp/Program.cs
+++ StarcraftBuildOrderApp/Program.cs
@@ -13,12 +13,16 @@ namespace StarcraftBuildOrderApp
         static void Main(string[] args)
         {
             
-            float score;
-            // do not include starting units: SCVs and Command Center
-            
+           
+           
+
+
+            cost.build_req.Add(unit_type.MARINE);
+            //cost.build_req.Add(unit_type.MARINE);
+
 			TabuSearch tabu = new TabuSearch (10, 50, new Solution ());
 			Solution s = tabu.iterate (tabu.bestSolution);
-			for (int i = 0; i < 500; i++) {
+			for (int i = 0; i < 1000; i++) {
 				s = tabu.iterate (s);
 			}
 
