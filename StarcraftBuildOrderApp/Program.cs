ffa7cf240477ccb60dff3b64af4c3ae2054c7997
Łukasz <Waligrski>
2014-09-13 11:04:53 +0200

    ulatwienie testow

--- StarcraftBuildOrderApp/Program.cs
+++ StarcraftBuildOrderApp/Program.cs
@@ -15,29 +15,40 @@ namespace StarcraftBuildOrderApp
             
 
             //initialize cost module
-
-            cost.fullfill_coef = (float)(1000);
-            cost.illegal_coef = (float)(10000);
-            cost.ovrload_coef = (float)(10000);
-         
-
-            cost.build_req.Add(unit_type.SUPPLY_DEPOT);
-            cost.build_req.Add(unit_type.MARINE);
-            cost.build_req.Add(unit_type.MARINE);
-            cost.build_req.Add(unit_type.MARINE);
-
-			TabuSearch tabu = new TabuSearch (10, 10, new Solution ());
-			Solution s = tabu.iterate (tabu.bestSolution);
-			for (int i = 0; i < 5000; i++) {
-				s = tabu.iterate (s);
-			}
-
-			Console.WriteLine("Score: " + tabu.bestSolutionValue);
-			tabu.bestSolution.printSolution ();
+           
+                cost.fullfill_coef = (float)(1000);
+                cost.illegal_coef = (float)(10000);
+                cost.ovrload_coef = (float)(10000);
 
 
-			Console.WriteLine ("KONIEC");
-            Console.ReadKey();
+                cost.build_req.Add(unit_type.SUPPLY_DEPOT);
+                cost.build_req.Add(unit_type.MARINE);
+                cost.build_req.Add(unit_type.MARINE);
+                cost.build_req.Add(unit_type.MARINE);
+                cost.build_req.Add(unit_type.GOLIATH);
+
+                TabuSearch tabu = new TabuSearch(10, 10, new Solution());
+                Solution s = tabu.iterate(tabu.bestSolution);
+
+
+            while (true)
+            {
+                for (int i = 0; i < 5000; i++)
+                {
+                    s = tabu.iterate(s);
+                }
+
+                Console.WriteLine("Score: " + tabu.bestSolutionValue);
+                tabu.bestSolution.printSolution();
+
+
+                Console.WriteLine("KONIEC");
+                if (Console.ReadKey().Key  == ConsoleKey.Enter)
+                {
+                    break;
+                }
+
+            }
         }
     }
 }
